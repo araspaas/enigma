553e5ce1fc57b5d81369402c78aa34fd0d285fbf
Austin Aspaas <araspaas@live.com>
2020-06-06 15:44:19 -0600

    Add create offset method and pass test

--- lib/offset.rb
+++ lib/offset.rb
@@ -8,4 +8,13 @@ class Offset
   def date_squared
     date.to_i * date.to_i
   end
+
+  def create_offset
+    {
+      A: date_squared.to_s[-4].to_i,
+      B: date_squared.to_s[-3].to_i,
+      C: date_squared.to_s[-2].to_i,
+      D: date_squared.to_s[-1].to_i
+    }
+  end
 end
