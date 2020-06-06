a5e6ca5475a59c575e0c48f347931ab9233375c5
Austin Aspaas <araspaas@live.com>
2020-06-05 23:56:44 -0600

    Add encrypt runner and decrypt runner. basic outline

---
+++ lib/decrypt.rb
@@ -0,0 +1,20 @@
+require_relative 'enigma'
+
+enigma = Enigma.new
+
+file = File.open(ARGV[0], "r")
+
+encrypted_message = file.read
+file.close
+
+key = ARGV[2]
+date = ARGV[3]
+
+decrypted_message = enigma.dycrypt(encrypted_message, key, date)
+
+write = File.open(ARGV[1], "w")
+
+write.write(decrypted_message)
+write.close
+
+p "Created #{ARGV[1]} with the key #{key} and date #{date}"
