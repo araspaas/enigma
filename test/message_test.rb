6decd76958ab6baad9405b9313f94e8f1f02b079
Austin Aspaas <araspaas@live.com>
2020-06-07 19:24:35 -0600

    Add decode message to multiple tests and add decrypt test

--- test/message_test.rb
+++ test/message_test.rb
@@ -2,20 +2,26 @@ require './test/test_helper'
 require './lib/message'
 require './lib/key'
 require './lib/offset'
+require './lib/decode_message'
 
 class MessageTest < Minitest::Test
   def setup
     @message = Message.new("hello world", "02715", "040895")
+    @decode_message = Message.new("keder ohulw", "02715", "040895")
   end
 
   def test_it_exists
     assert_instance_of Message, @message
+    assert_instance_of Message, @decode_message
   end
 
   def test_it_has_attributes
     assert_equal "hello world", @message.message
     assert_equal "02715", @message.key
     assert_equal "040895", @message.date
+    assert_equal "keder ohulw", @decode_message.message
+    assert_equal "02715", @decode_message.key
+    assert_equal "040895", @decode_message.date
   end
 
   def test_it_has_the_alphabet
@@ -30,19 +36,26 @@ class MessageTest < Minitest::Test
   def test_keys
     expected = { A: 2, B: 27, C: 71, D: 15}
     assert_equal expected, @message.keys
+    assert_equal expected, @decode_message.keys
   end
 
   def test_offsets
     expected = {A: 1, B: 0, C:2, D: 5}
+    assert_equal expected, @message.offsets
     assert_equal expected, @message.offsets
   end
 
   def test_it_can_shift
     expected = {A: 3, B: 0, C: 19, D: 20}
     assert_equal expected, @message.shifts
+    assert_equal expected, @decode_message.shifts
   end
 
   def test_it_can_encrypt
     assert_equal "keder ohulw", @message.encrypt_message
+  end
+
+  def test_it_can_decrypt
+    assert_equal "hello world", @decode_message.decrypt_message
   end
 end
