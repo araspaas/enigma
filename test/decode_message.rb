02cdc0e6522c43381bfdb53f5a93e438a520e9a2
Austin Aspaas <araspaas@live.com>
2020-06-07 18:32:16 -0600

    Add alphabet test

--- test/decode_message.rb
+++ test/decode_message.rb
@@ -15,4 +15,9 @@ class DecodeMessageTest < Minitest::Test
     assert_equal "02715", @decode_message.key
     assert_equal "040895", @decode_message.date
   end
+
+  def test_it_has_the_alphabet
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @decode_message.alphabet
+  end
 end
