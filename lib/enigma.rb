acd6722f4bb6c0e28969d43ae9318f697571a42e
Austin Aspaas <araspaas@live.com>
2020-06-08 14:47:47 -0600

    Add decrypt method to enigma class and pass test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,4 +8,9 @@ class Enigma
     new_message = Message.new(message, key, date)
     new_message.encrypt
   end
+
+  def decrypt(message, key, date)
+    new_message = Message.new(message, key, date)
+    new_message.decrypt
+  end
 end
